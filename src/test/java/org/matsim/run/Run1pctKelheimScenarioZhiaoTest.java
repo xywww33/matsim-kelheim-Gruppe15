bc107d5324ce23f70453b4c2fcdfdb6e079414f6
Zhiao Yang <zhiao.yang@campus.tu-berlin.de>
2025-12-04 11:48:04 +0100

    Commit Zhiao Test

---
+++ src/test/java/org/matsim/run/Run1pctKelheimScenarioZhiaoTest.java
@@ -0,0 +1,16 @@
+package org.matsim.run;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Run1pctKelheimScenarioZhiaoTest {
+	@Test
+	public void testMain(){
+		String [] args = {"input/v3.1/kelheim-v3.1-config.xml",
+			"--config:controller.lastIteration","1"
+		};
+		Run1pctKelheimScenario.main(args);
+	}
+
+}
